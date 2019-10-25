b1642206fa74d39e0aea1bd3800a0ea6023b67b6
Chris Neal <voodoodaddy2000@gmail.com>
2019-10-25 14:34:41 -0700

    Save Point

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 var apiKey = "dbf364bc39a4580a03a0dd92d999aa37";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=" + apiKey;
+var currentCityToSearch = "Tempe"
+var currentWxQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCityToSearch + "&units=imperial&appid=" + apiKey;
+var currentForecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + currentCityToSearch + "&units=imperial&appid=" + apiKey;
+var currentUVQueryURL = "";
 var $currentCityDisplay = $("#current-city-display");
 var $currentDateDisplay = $("#current-date-display");
 var $currentTempDisplay = $("#current-temp-display");
@@ -7,18 +10,59 @@ var $currentHumidityDisplay = $("#current-humidity-display");
 var $currentWindDisplay = $("#current-wind-display");
 var $currentUvDisplay = $("#current-uv-display");
 var $wxIcon = $("#wx-icon");
+var temp = new Date();
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    console.log(Date(response.dt));
-    $currentCityDisplay.text(response.name);
-    // $currentDateDisplay.text(Date(response.dt));
-    $currentTempDisplay.text(response.main.temp);
-    $currentHumidityDisplay.text(response.main.humidity);
-    $currentWindDisplay.text(response.wind.speed);
-    $currentUvDisplay.text(response.name);
-    $wxIcon.attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
-  });
+function getWx() {
+    $.ajax({
+        url: currentWxQueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var thisDate = new Date(response.dt * 1000);
+        $currentCityDisplay.text(response.name);
+        $currentDateDisplay.text(thisDate.getDate() + "/" + (thisDate.getMonth() + 1) + "/" + thisDate.getFullYear());
+        $currentTempDisplay.text(response.main.temp);
+        $currentHumidityDisplay.text(response.main.humidity);
+        $currentWindDisplay.text(response.wind.speed);
+        $currentUvDisplay.text(response.name);
+        $wxIcon.attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+        console.log(response.coord.lat, response.coord.lon);
+        getForecast();
+        getUvIndex(response.coord.lat, response.coord.lon);
+    });
+}
+
+function getForecast() {
+    $.ajax({
+        url: currentForecastQueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // var thisDate = new Date(response.dt * 1000);
+        // $currentCityDisplay.text(response.name);
+        // $currentDateDisplay.text(thisDate.getDate() + "/" + (thisDate.getMonth() + 1) + "/" + thisDate.getFullYear());
+        // $currentTempDisplay.text(response.main.temp);
+        // $currentHumidityDisplay.text(response.main.humidity);
+        // $currentWindDisplay.text(response.wind.speed);
+        // $currentUvDisplay.text(response.name);
+        // $wxIcon.attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+        // console.log(response.coord.lat, response.coord.lon);
+        // getForecast(response.coord.lat, response.coord.lon);
+        // getUvIndex(response.coord.lat, response.coord.lon);
+    });
+}
+
+function getUvIndex(lat, lon) {
+    currentUVQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+    $.ajax({
+        url: currentUVQueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $currentUvDisplay.text(response.value);
+    });
+}
+
+$(document).ready(function () {
+    getWx();
+})
