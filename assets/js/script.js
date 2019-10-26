84736c0cc868af1633b0b2506e0fd5a090bac6b3
Chris Neal <voodoodaddy2000@gmail.com>
2019-10-25 20:11:20 -0700

    UV Index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var apiKey = "dbf364bc39a4580a03a0dd92d999aa37";
-var currentCityToSearch = "Tempe"
+var currentCityToSearch = "Honolulu"
 var currentWxQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCityToSearch + "&units=imperial&appid=" + apiKey;
 var currentForecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCityToSearch + "&units=imperial&appid=" + apiKey;
 var currentUVQueryURL = "";
@@ -17,16 +17,14 @@ function getWx() {
         url: currentWxQueryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        var thisDate = new Date(response.dt * 1000);
+        var thisDate = response.dt;// - response.timezone;
         $currentCityDisplay.text(response.name);
-        $currentDateDisplay.text(thisDate.getDate() + "/" + (thisDate.getMonth() + 1) + "/" + thisDate.getFullYear());
+        $currentDateDisplay.text(moment.unix(thisDate).format("DD/MM/YYYY"));
         $currentTempDisplay.text(response.main.temp);
         $currentHumidityDisplay.text(response.main.humidity);
         $currentWindDisplay.text(response.wind.speed);
         $currentUvDisplay.text(response.name);
         $wxIcon.attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
-        console.log(response.coord.lat, response.coord.lon);
         getForecast();
         getUvIndex(response.coord.lat, response.coord.lon);
     });
@@ -58,8 +56,23 @@ function getUvIndex(lat, lon) {
         url: currentUVQueryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
         $currentUvDisplay.text(response.value);
+        switch (true) {
+            case (response.value < 3):
+                $currentUvDisplay.addClass("uv-12");
+                break;
+            case response.value < 6:
+                $currentUvDisplay.addClass("uv-35");
+                break;
+            case response.value < 8:
+                $currentUvDisplay.addClass("uv-67");
+                break;
+            case response.value < 11:
+                $currentUvDisplay.addClass("uv-810");
+                break;
+            default:
+                $currentUvDisplay.addClass("uv-11");
+        }
     });
 }
 
